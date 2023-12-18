3652190f2f9558fcdde3f6b106909a4d8087eeac
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-12-18 18:03:04 +0200

    added project

---
+++ WebApplication1/Pages/Contact.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages;
+
+public class ContactModel : PageModel
+{
+  public void OnGet()
+  {
+    Console.WriteLine("I am running");
+  }
+}
+
